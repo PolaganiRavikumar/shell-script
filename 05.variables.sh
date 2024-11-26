6f42d0b06db1437888eaa3ea97783d39bec09868
Ravikumar <ravindra954@gmail.com>
2024-11-26 14:50:10 +0530

    secure the password

---
+++ 05.variables.sh
@@ -0,0 +1,7 @@
+#! bin/bash
+echo " please enter your username::"
+read -s USERNAME: 
+echo "user name entered is:$USERNAME"
+echo "please enter your password"
+read -s PASSWORD:
+echo "user password is :$PASSWORD"
