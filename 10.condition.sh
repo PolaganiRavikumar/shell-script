80a3972dac181c95edd71be1e5768ead092c76bb
Ravikumar <ravindra954@gmail.com>
2024-11-26 15:36:51 +0530

    conditional statement

--- 10.condition.sh
+++ 10.condition.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-Number=$1
-if [ $Number -gt 20 ]
+NUMBER=$1
+if [ $NUMBER -gt 20 ]
 then
-    echo "Given number is : $Number is greater than 20"
+    echo "Given number is : $NUMBER is greater than 20"
 else 
-    echo "Given number is : $Number is less than 20"
+    echo "Given number is : $NUMBER is less than 20"
 fi
