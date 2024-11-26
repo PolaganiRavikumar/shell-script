12f82ab9662e986275da4f1a901d47c98bb7f204
Ravikumar <ravindra954@gmail.com>
2024-11-26 15:56:26 +0530

    package installation

---
+++ 11.installation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+    echo "please run the script with root priviliges"
+    exit 1
+fi
+dnf list installed git 
+if [ $? -ne 0 ]
+then
+    echo "git is not installed, going to install it.."
+    dnf install git -y
+if [ $? -ne 0 ]
+then
+    echo "git installation is not success....check it"
+    exit 1
+else
+    echo "git installation is success"
+fi
