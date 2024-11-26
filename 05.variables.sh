9bbf671e45a182c07c78bfa8d37a514b3d0af5cd
Ravikumar <ravindra954@gmail.com>
2024-11-26 14:51:11 +0530

    secure username password

--- 05.variables.sh
+++ 05.variables.sh
@@ -2,6 +2,6 @@
 echo " please enter your username::"
 read -s USERNAME: 
 echo "user name entered is:$USERNAME"
-echo "please enter your password"
+echo "please enter your password::"
 read -s PASSWORD:
 echo "user password is :$PASSWORD"
