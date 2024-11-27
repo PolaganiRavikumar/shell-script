eab815604ac9c366e6f71ee7173d16823d9c9f3e
Ravikumar <ravindra954@gmail.com>
2024-11-27 10:44:08 +0530

    delete old 14days log files

---
+++ 18.delete-old.log.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+SOURCE_DIR= /home/ec2-user/logs
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ -d $SOURCE_DIR ]
+then
+    echo -e "$SOURCE_DIR $G exits $N"
+else
+    echo -e "$SOURCE_DIR $R doesnot exits $N"
+    exit 1
+fi 
+
+FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime+14)
+echo "Files: $FILES"
+
+While IFS= read -r file
+do
+    echo "deleting file:$file"
+    rm -rf $file
+done <<< $FILES
